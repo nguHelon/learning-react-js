87b969730107d17051af02edbfe6bae495a6ae29
Ngu Helon <nguhelon@gmail.com>
2023-03-02 21:25:25 +0100

    learning more about state and enhancing the meme generator project code

--- react-memeGenerator-project3/src/components/Meme.js
+++ react-memeGenerator-project3/src/components/Meme.js
@@ -2,14 +2,27 @@ import React from "react";
 import memesArray from "../memesData";
 
 const Meme = () => {
-    let [memeImage, setMemeImage] = React.useState("");
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesArray);
+
     function preventSubmission(e) {
         e.preventDefault();
     }
 
     function getMemeImage() {
         let random = Math.floor(Math.random() * memesArray.length);
-        setMemeImage(memesArray[random].url);
+        let url = memesArray[random].url
+        setMeme((prevMeme) => {
+            return ({
+                ...prevMeme,
+                randomImage: url
+            })
+        });
     }
 
     return (
@@ -21,7 +34,7 @@ const Meme = () => {
                 </div>
                 <button onClick={getMemeImage}>Get a new image 🔲 </button>
             </form>
-            <img src={memeImage} alt="" />
+            <img src={meme.randomImage} alt="" />
         </div>
     )
 }
